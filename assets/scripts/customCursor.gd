a8971abd24214f30be629b302f22fd8c0c195b19
Verwex <34553484+Verwex@users.noreply.github.com>
2025-04-03 20:33:15 +0200

    custom cursor

---
+++ assets/scripts/customCursor.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Load the custom images for the mouse cursor.
+var arrow = load("res://assets/images/cursor/arrow.png")
+var hand = load("res://assets/images/cursor/hand.png")
+
+func _ready():
+	# Changes only the arrow shape of the cursor.
+	# This is similar to changing it in the project settings.
+	Input.set_custom_mouse_cursor(arrow)
+
+	# Changes a specific shape of the cursor (here, the I-beam shape).
+	Input.set_custom_mouse_cursor(hand, Input.CURSOR_POINTING_HAND)
