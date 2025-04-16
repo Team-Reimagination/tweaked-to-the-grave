2f1fed3850baf9b94b17a39b27b23debdaded896
Verwex <34553484+Verwex@users.noreply.github.com>
2025-04-17 01:15:31 +0200

    implement Lira as a collectible

---
+++ assets/scripts/classes/collectible_class.gd
@@ -0,0 +1,47 @@
+class_name TTTG_Collectible
+extends TTTG_Entity
+
+@export var objType : String
+@export var value : int
+
+var startSucking = false
+var willDisappear = false
+
+func _ready() -> void:
+	super._ready()
+	
+	$Model.get_child(1).play('default')
+	
+	self.area_shape_entered.connect(detectCollission.bind())
+
+func suicide():
+	if objType == 'lira':
+		scene.TRUElira += value;
+		scene.liraPGR += value;
+		
+		willDisappear = true
+		startSucking = false
+		
+		$CollectAudio.play()
+		
+		overridePos = global_position
+		$Model.get_child(1).visible = false
+		$Model.get_child(0).visible = true
+		$Model.get_child(0).play('default')
+
+var ole = 0.0;
+
+func _process(delta: float) -> void:
+	super._process(delta)
+	
+	if willDisappear and $Model.get_child(0).is_playing() == false: queue_free()
+	
+	if startSucking:
+		ole += delta/2;
+		self.global_position.x = lerpf(self.global_position.x, scene.player.global_position.x, ole)
+		self.global_position.y = lerpf(self.global_position.y, scene.player.global_position.y, ole)
+		self.global_position.z = lerpf(self.global_position.z, scene.player.global_position.z, ole)
+
+func detectCollission(_areID, are, _arSID, _loSID):
+	if are.type == 'player' and _loSID == 0 and !startSucking: startSucking = true
+	elif are.type == 'player_collector' and _loSID == 1 and !willDisappear: suicide()
--- assets/scripts/classes/entity_class.gd
+++ assets/scripts/classes/entity_class.gd
@@ -2,11 +2,13 @@ class_name TTTG_Entity
 extends Area3D
 
 @onready var scene = get_parent().get_parent()
-@onready var model = $Model;
-@onready var hitbox = $Hitbox;
-@export var type = "";
+var model;
 
+@export var type = "";
 @export var doProcessDistanceFade = true
+@export var hasModel = true
+@export var overridePos : Vector3 = Vector3(-INF,-INF,-INF)
+
 var distanceFadePhase = 0;
 var isReady : bool = false
 
@@ -14,10 +16,13 @@ var meshInstances = []
 var meshesWithOverlay = []
 
 func _ready() -> void:
-	meshInstances = model.find_children("*", "MeshInstance3D", true, true).filter(func(x): return x.material_overlay != null)
-	meshesWithOverlay = model.find_children("*", "MeshInstance3D", true, true)
+	if hasModel:
+		model = $Model
 	
 	if doProcessDistanceFade and PlayGlobals.levelDefs != null:
+		meshInstances = model.find_children("*", "MeshInstance3D", true, true).filter(func(x): return x.material_overlay != null)
+		meshesWithOverlay = model.find_children("*", "MeshInstance3D", true, true)
+	
 		for a in meshesWithOverlay:
 			a.material_overlay.set("shader_parameter/fade_start", PlayGlobals.getDistance())
 			a.material_overlay.set("shader_parameter/fade_end", PlayGlobals.getDistance() - 100)
@@ -27,6 +32,9 @@ func _ready() -> void:
 			a.mesh.surface_get_material(0).distance_fade_max_distance = PlayGlobals.getDistance() - 100
 
 func _process(_delta: float) -> void:
+	if overridePos != Vector3(-INF,-INF,-INF):
+		self.global_position = overridePos
+	
 	if self.global_position.z > 100 and type != 'boss':
 			self.queue_free()
 	
