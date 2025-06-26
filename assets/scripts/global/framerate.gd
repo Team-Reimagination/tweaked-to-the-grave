f937e199619caeb6b11e1f2335fe3b78043cef6b
Verwex <34553484+Verwex@users.noreply.github.com>
2025-06-26 09:12:47 +0200

    framerate counter, will be for max framerate later

---
+++ assets/scripts/global/framerate.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+var framerate: Label = Label.new()
+
+func _ready() -> void:
+	self.layer = 128
+	
+	framerate.top_level = true
+	framerate.label_settings = LabelSettings.new()
+	framerate.label_settings.font = load("res://assets/fonts/CrookedLogs.ttf")
+	framerate.label_settings.font_size = 16
+	
+	framerate.position = Vector2(10, 10)
+	
+	add_child(framerate)
+	
+func _process(_delta: float) -> void:
+	var fps_count = Performance.get_monitor(Performance.TIME_FPS)
+	framerate.text = str(fps_count) + " FPS"
